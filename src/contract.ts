8d8ffad24be80ebe8d8b87d1624d083b6dfa6d62
Sushruth Sastry <shastry.sushruth@gmail.com>
2025-03-27 13:26:50 -0700

    chore: remove vite and use 1fe-cli for all widgets dev and build commands

--- src/contract.ts
+++ src/contract.ts
@@ -1,7 +1,6 @@
-/* eslint-disable @typescript-eslint/no-empty-interface */
-
 import { PlatformPropsType } from '@devhub/1fe-shell';
 
+/* eslint-disable @typescript-eslint/no-empty-interface */
 export interface HostPropsContract {}
 
 export type WidgetProps = {
