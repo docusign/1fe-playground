f128349c4bf31df20d632b92d7d7a7268f58b667
Luke Hatcher <lukehatcher98@gmail.com>
2025-03-28 15:04:45 -0700

    fix: prop drilling, prop types and misc clean up

--- src/contract.ts
+++ src/contract.ts
@@ -1,26 +1,10 @@
 /* eslint-disable @typescript-eslint/no-empty-interface */
 
-type PlatformProps = {
-  /*
-   * NOTE: This is a placeholder for the utils type
-   * A more formal definition will be available in the future.
-   */
-  utils: Record<string, unknown>;
-  context: {
-    self: {
-      widgetId: string;
-      version: string;
-    };
-  };
-  environment: string;
-};
+import { PlatformPropsType } from '@devhub/1fe-shell';
 
-// Widget starter kit is a plugin and does not expect any host props.
-// The empty interface still results in a zod schema that is used to validate that no host props were sent
-// export const hostPropsContractSchema = z.object({});
 export interface HostPropsContract {}
 
 export type WidgetProps = {
-  platform: PlatformProps;
+  platform: PlatformPropsType;
   host?: HostPropsContract;
 };
