34d3d7416f9ec3f29b9c3ea6187cf453bdbf85ab
Chaitanya Varma <chaitanya.varma@docusign.com>
2025-07-11 12:52:24 -0700

    simple pw test

--- tests/widgetbrowser.spec.ts
+++ tests/widgetbrowser.spec.ts
@@ -1,11 +1,8 @@
 import { test, expect } from "@playwright/test";
 import packageJson from "../package.json" with { type: "json" };
 
-const version = process.env.WIDGET_VERSION || packageJson.version;
-const widgetId = process.env.WIDGET_ID || packageJson.name;
-
 test("should load the bathtub page successfully", async ({ page }) => {
-  const url = `https://demo.1fe.com/bathtub?widgetUrl=https://1fe-a.akamaihd.net/integration/widgets/${widgetId}/${version}/js/1fe-bundle.js&fixPreview=true`;
+  const url = `https://demo.1fe.com/bathtub`;
   
   await page.goto(url);
   await page.waitForLoadState('networkidle');
